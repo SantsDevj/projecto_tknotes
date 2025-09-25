7431e492205bb0932e695eed6dd862da10431da6
Santos Eduardo Júnior <santoseduardojuniors01@gmail.com>
2025-09-25 18:59:21 +0200

    commitando mudanças

--- scripts_db_tknotes/db_tknotes.sql
+++ scripts_db_tknotes/db_tknotes.sql
@@ -5,23 +5,14 @@ collate utf8mb3_general_ci;
 use tknotes;
 create table tarefas(
 id_tarefa int auto_increment,
-titulo varchar(30),
+titulo varchar(255),
 descricao text,
 data_venc date,
-categoria varchar(30),
-prioridade varchar (50),
+categoria varchar(100),
+prioridade varchar (20),
 concluida boolean,
 primary key(id_tarefa)
 )default charset = utf8mb3;
-
-alter table tarefas 
-modify column titulo varchar(255);
-
-alter table tarefas 
-modify column categoria varchar(100);
-
-alter table tarefas
-modify column prioridade varchar(20);
 
 
 create table usuario(
@@ -30,12 +21,4 @@ nome_usuario varchar(30) unique,
 senha varchar(8)
 );
 
-show tables;
-
-
-alter table user
-rename to usuario;
-
-desc usuario;
-desc tarefas;
 
